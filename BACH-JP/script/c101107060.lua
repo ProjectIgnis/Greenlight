f4bd4e4b3ff18764d1e213c9798665bdf4a0a232
Dylam De La Torre <DyXel04@gmail.com>
2021-10-10 16:29:30 +0200

    Update c101107060.lua

--- BACH-JP/script/c101107060.lua
+++ BACH-JP/script/c101107060.lua
@@ -1,3 +1,4 @@
+--ふわんだりぃずと旅じたく
 --Floowandereeze and the Journey Preparations
 --Scripted by Zefile
 local s,id=GetID()
@@ -6,7 +7,6 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_RECOVER)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetCost(s.cost)
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 end
 s.listed_series={0x16f}
 function s.filter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_WINDBEAST) and c:IsAbleToRemoveAsCost()
+	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_WINGEDBEAST) and c:IsAbleToRemoveAsCost()
 end
 function s.filteradd(c)
 	return c:IsSetCard(0x16f) and (c:IsType(TYPE_MONSTER) or c:IsType(TYPE_FIELD)) and c:IsAbleToHand()
@@ -34,12 +34,9 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.filteradd,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+	if #g==0 then return end
+	Duel.SendtoHand(g,nil,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,g)
 	Duel.BreakEffect()
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(1200)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,1200)
+	Duel.Recover(tp,500,REASON_EFFECT)
 end
