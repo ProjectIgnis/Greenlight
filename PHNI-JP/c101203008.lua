0b5e0b529f438ee9aaa87f20d4e503dc960fe1a4
Lilac <107518574+Satellaa@users.noreply.github.com>
2023-10-09 12:25:16 +0700

    Add `and`

--- PHNI-JP/c101203008.lua
+++ PHNI-JP/c101203008.lua
@@ -43,7 +43,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 end
 function s.thfilter(c)
-	return c:IsSetCard(SET_GOBLIN_RIDER) and c:IsSpellTrap() c:IsAbleToHand()
+	return c:IsSetCard(SET_GOBLIN_RIDER) and c:IsSpellTrap() and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
