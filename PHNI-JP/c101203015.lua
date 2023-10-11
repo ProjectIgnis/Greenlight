49642a6fef8b92a58b14bd08c0d491a3feb811a6
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-11 16:23:06 +0300

    Ghoti + Fishlamp updates/fixes

--- PHNI-JP/c101203015.lua
+++ PHNI-JP/c101203015.lua
@@ -10,7 +10,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcon)
+	e1:SetCondition(function() return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_FISH),0,LOCATION_MZONE,LOCATION_MZONE,1,nil) end)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
@@ -19,8 +19,8 @@ function s.initial_effect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetTarget(s.rmvtg)
@@ -34,13 +34,10 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e3:SetRange(LOCATION_REMOVED)
 	e3:SetCountLimit(1,{id,2})
-	e3:SetCondition(s.selfspcond)
-	e3:SetTarget(s.selfsptg)
-	e3:SetOperation(s.selfspop)
+	e3:SetCondition(function(e) return Duel.GetTurnCount()==e:GetHandler():GetTurnID()+1 end)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
-end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_FISH),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -62,41 +59,28 @@ function s.fishspfilter(c,e,tp)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.rmvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
 	local c=e:GetHandler()
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,eg:GetFirst():GetLevel(),e,tp) end
-	if chk==0 then return c:IsAbleToRemove()
+	if chk==0 then return c:IsAbleToRemove() and Duel.GetMZoneCount(tp,c)>0
 		and Duel.IsExistingTarget(s.rmvfilter,tp,0,LOCATION_MZONE,1,nil,eg)
-		and Duel.IsExistingTarget(s.fishspfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp)
-		and Duel.GetMZoneCount(tp,c)>0 end
+		and Duel.IsExistingTarget(s.fishspfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local rg=Duel.SelectTarget(tp,s.rmvfilter,tp,0,LOCATION_MZONE,1,1,nil,eg)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sg=Duel.SelectTarget(tp,s.fishspfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
 	rg:AddCard(c)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,rg,#rg,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,rg,2,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,1,tp,0)
 end
 function s.rmvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	local g=Duel.GetTargetCards(e)
+	if #g==0 then return end
 	local rg,sg=g:Split(Card.IsControler,nil,1-tp)
+	if #rg==0 then return end
 	rg:AddCard(c)
-	if #rg==2 and Duel.Remove(rg,nil,REASON_EFFECT)==2 then
+	if Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)==2 and #sg>0 then
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
-end
-function s.selfspcond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnCount()==e:GetHandler():GetTurnID()+1
-end
-function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_REMOVED)
-end
-function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 end
--- PHNI-JP/c101203044.lua
+++ PHNI-JP/c101203044.lua
@@ -11,10 +11,10 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_BE_MATERIAL)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_BE_MATERIAL)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.tkncond)
+	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return e:GetHandler():IsLocation(LOCATION_GRAVE) and r&REASON_SYNCHRO>0 end)
 	e1:SetTarget(s.tkntg)
 	e1:SetOperation(s.tknop)
 	c:RegisterEffect(e1)
@@ -26,41 +26,39 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCost(aux.bfgcost)
 	e2:SetCondition(s.descon)
+	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(s.destg)
 	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 end
-s.listed_names={id+1} --Lamp Tokens
-function s.tkncond(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r&REASON_SYNCHRO>0
-end
+s.listed_names={id+100} --Lamp Token
 function s.tkntg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>=2
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,2,RACE_FISH,ATTRIBUTE_FIRE,POS_FACEUP) end
-	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,0,0)
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0,TYPES_TOKEN,0,0,2,RACE_FISH,ATTRIBUTE_FIRE) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,0)
 end
 function s.tknop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	--Cannot Special Summon monsters from the Extra Deck, except Fish monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetTarget(function(e,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsRace(RACE_FISH) end)
 	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(_e,_c) return _c:IsLocation(LOCATION_EXTRA) and not _c:IsRace(RACE_FISH) end)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	--Clock Lizard check
-	aux.addTempLizardCheck(c,tp,function(e,c) return not c:IsOriginalRace(RACE_FISH) end)
+	aux.addTempLizardCheck(c,tp,function(_e,_c) return not _c:IsOriginalRace(RACE_FISH) end)
 	--Special Summon 2 "Lamp Tokens"
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2
-		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,2,RACE_FISH,ATTRIBUTE_FIRE,POS_FACEUP) then return end
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0,TYPES_TOKEN,0,0,2,RACE_FISH,ATTRIBUTE_FIRE) then return end
 	for i=1,2 do
-		local token=Duel.CreateToken(tp,id+i)
+		local token=Duel.CreateToken(tp,id+100)
 		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
 	end
 	Duel.SpecialSummonComplete()
