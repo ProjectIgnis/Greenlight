25d5026d6cdc4608f7adad16a938a2f4b48dda28
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-11-09 21:14:56 -0800

    Add "Stronghold the Hidden Fortress"

---
+++ LEDE-JP/c101204071.lua
@@ -0,0 +1,84 @@
+--JP Name
+--Stronghold the Hidden Fortress
+--Scripted by The Razgriz
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_LIGHT_SARC}
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and 
+		Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,TYPE_EFFECT|TYPE_MONSTER,0,2000,4,RACE_MACHINE,ATTRIBUTE_EARTH) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,TYPE_EFFECT|TYPE_MONSTER,0,2000,4,RACE_MACHINE,ATTRIBUTE_EARTH) then return end
+	c:AddMonsterAttribute(TYPE_EFFECT|TYPE_TRAP)
+	Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
+	c:AddMonsterAttributeComplete()
+	--Gains 1000 ATK for each "Gold Sarcophagus of Light" and monsters that mention it
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.val)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	c:RegisterEffect(e1,true)
+	--Destroy attacking monster if you control "Gold Sarcophagus of Light" and monster that mentions
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.descon)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+	c:RegisterEffect(e2,true)
+	Duel.SpecialSummonComplete()
+end
+--ATK gain functions
+function s.gslfilter(c)
+	return c:IsCode(CARD_LIGHT_SARC) or (c:IsMonster() and c:IsFaceup() and c:ListsCode(CARD_LIGHT_SARC))
+function s.val(e,c)
+	local tp=e:GetHandlerPlayer()
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0):Filter(s.gslfilter,nil)
+	return #g*1000 
+end
+--Destroy functions
+function s.desfilter(c,tp)
+	return c:IsCode(CARD_LIGHT_SARC) and Duel.IsExistingMatchingCard(s.desfilter2,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.desfilter2(c)
+	return c:IsMonster() and c:IsFaceup() and c:ListsCode(CARD_LIGHT_SARC)
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local at=Duel.GetAttacker()
+	return at:IsControler(1-tp) and Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_MZONE,0,1,nil,tp)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local at=Duel.GetAttacker()
+	if chk==0 then return at:IsRelateToBattle() end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,at,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local at=Duel.GetAttacker()
+	if at:IsRelateToBattle() then
+		Duel.Destroy(at,REASON_EFFECT)
+	end
+end
