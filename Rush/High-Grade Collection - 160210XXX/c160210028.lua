c796782cbb2fd0151dead19e0b3ff43611ceff5a
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-12-15 10:43:42 +0100

    added Eternal Wolf of the Blaze Fiends

--- Rush/High-Grade Collection - 160210XXX/c160210028.lua
+++ Rush/High-Grade Collection - 160210XXX/c160210028.lua
@@ -5,6 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TODECK)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
