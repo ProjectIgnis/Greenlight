fe0d181d8bcd6d9774df68dd4da78c791ee16e80
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-12-17 15:48:13 +0100

    added Jinzo the Machine Menace

--- Rush/Transamu of Transcendence - 160016XXX/c160016028.lua
+++ Rush/Transamu of Transcendence - 160016XXX/c160016028.lua
@@ -1,12 +1,20 @@
---人造人間－サイコ・リターナー
---Jinzo - Returner
+--脅威の人造人間－サイコ・ショッカー
+--Jinzo the Machine Menace
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
+	--special summon
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCondition(s.spcon)
+	c:RegisterEffect(e0)
 	--Special Summon 1 "Jinzo"
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON|CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -16,6 +24,11 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_names={CARD_JINZO}
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and	Duel.IsExistingMatchingCard(Card.IsTrap,c:GetControler(),LOCATION_GRAVE,LOCATION_GRAVE,1,nil)
+end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 end
@@ -34,7 +47,12 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	--Effect
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 and Duel.IsExistingMatchingCard(Card.IsFacedown,tp,0,LOCATION_STZONE,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local sg=Duel.GetMatchingGroup(Card.IsFacedown,tp,0,LOCATION_STZONE,nil)
+		Duel.ConfirmCards(tp,sg)
+		local dg=sg:Filter(Card.IsTrap,nil)
+		if #dg>0 then
+			Duel.Destroy(dg,REASON_EFFECT)
+		end
 	end
 end
