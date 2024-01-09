66e7685547205ad8893497eb207fa7037060d601
Naim <naimsantos45@gmail.com>
2024-01-09 11:50:25 -0300

    Ultimate Flame Swordsman

--- MZMI/c100434007.lua
+++ MZMI/c100434007.lua
@@ -30,6 +30,7 @@ function s.initial_effect(c)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
 end
+s.listed_names={45231177} --Flame Swordsman
 function s.eqfilter(c)
 	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_WARRIOR)
 end
