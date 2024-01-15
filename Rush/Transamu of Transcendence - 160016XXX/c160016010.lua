414935670f55d77d401b4f62e56afe48854f62b1
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-01-15 19:01:59 +0100

    added Dice Demon

--- Rush/Transamu of Transcendence - 160016XXX/c160016010.lua
+++ Rush/Transamu of Transcendence - 160016XXX/c160016010.lua
@@ -1,12 +1,12 @@
---サイコロの天使
---Dice Angel
+--サイコロの悪魔
+--Dice Demon
 --Scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
 	--Add card to hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND|CATEGORY_DICE|CATEGORY_RECOVER)
+	e1:SetCategory(CATEGORY_TOHAND|CATEGORY_DICE|CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -16,20 +16,20 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.roll_dice=true
-s.listed_names={74137509}
+s.listed_names={126218}
 function s.tgfilter(c)
-	return c:IsAbleToGraveAsCost() and not c:IsCode(74137509)
+	return c:IsAbleToGraveAsCost() and not c:IsCode(126218)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_HAND,0,1,nil) end
 end
 function s.thfilter(c)
-	return c:IsCode(74137509) and c:IsAbleToHand()
+	return c:IsCode(126218) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	--Requirement
@@ -44,9 +44,9 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 		if Duel.GetFieldGroupCountRush(tp,0,LOCATION_MZONE)>1 then
 			local d=Duel.TossDice(tp,1)
-			if d~=6 then
+			if d~=1 then
 				Duel.BreakEffect()
-				Duel.Recover(tp,600,REASON_EFFECT)
+				Duel.Damage(1-tp,600,REASON_EFFECT)
 			end
 		end
 	end
