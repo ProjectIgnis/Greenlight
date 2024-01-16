694baca81da32832d947468e6c5e6c2d131b1039
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-01-16 13:53:46 +0100

    fixed Prime Pierce Giant

--- Rush/Transamu of Transcendence - 160016XXX/c160016037.lua
+++ Rush/Transamu of Transcendence - 160016XXX/c160016037.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Fusion Procedure
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,1600160001,1,s.ffilter,1)
+	Fusion.AddProcMixN(c,true,true,160016001,1,s.matfilter,1)
 	--pierce
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -17,13 +17,19 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetCondition(s.cond)
+	e2:SetValue(1)
 	c:RegisterEffect(e2)
 end
+s.named_material={160016001}
+function s.matfilter(c,scard,sumtype,tp)
+	return c:IsAttribute(ATTRIBUTE_LIGHT,scard,sumtype,tp) and c:IsLevelBelow(8)
+end
 function s.target(e,c)
-	return c:IsLevelBelow(8) and c:IsAttribute(ATTRIBUTE_LIGHT)
+	return c:IsLevelAbove(7) and c:IsAttribute(ATTRIBUTE_LIGHT)
 end
 function s.cond(e)
 	return Duel.IsTurnPlayer(1-e:GetHandlerPlayer())
