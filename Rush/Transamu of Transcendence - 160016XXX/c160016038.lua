76d65b18ca30716f06bae53963ce6891d64992a4
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-01-16 14:00:10 +0100

    fixed Prime Cat Straynya

--- Rush/Transamu of Transcendence - 160016XXX/c160016038.lua
+++ Rush/Transamu of Transcendence - 160016XXX/c160016038.lua
@@ -18,12 +18,13 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetTarget(s.tg)
 	e2:SetValue(200)
 	c:RegisterEffect(e2)
 end
+s.named_material={160016004}
 function s.tg(e,c)
 	return c:IsFaceup() and c:IsLevelAbove(7) and c:IsAttribute(ATTRIBUTE_LIGHT)
 end
