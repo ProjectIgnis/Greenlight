92a277a3d2bc684e32d95b0221e6f7a621353ac1
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-01-16 13:02:17 +0100

    fixed Prime Phantomite

--- Rush/Transamu of Transcendence - 160016XXX/c160016002.lua
+++ Rush/Transamu of Transcendence - 160016XXX/c160016002.lua
@@ -21,7 +21,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE,0,2,nil) end
 end
 function s.posfilter(c)
-	return c:IsLevelAbove(7) and c:IsCanTurnSet() and c:IsCanChangePositionRush()
+	return c:IsLevelAbove(7) and c:IsAttackPos() and c:IsCanTurnSet() and c:IsCanChangePositionRush()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,0,LOCATION_MZONE,1,nil) 
@@ -34,16 +34,17 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_GRAVE,0,2,2,nil)
 	Duel.HintSelection(g,true)
 	if Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)<1 then return end
+	Duel.ShuffleDeck(tp)
 	--Effect
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.HintSelection(g,true)
 	if #g>0 and Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)>0 then
-		if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=2 then
+		if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=3 then
 			Duel.ConfirmDecktop(tp,3)
 			Duel.SortDecktop(tp,tp,3)
 		end
-		if Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>=2 then
+		if Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>=3 then
 			Duel.ConfirmDecktop(1-tp,3)
 			Duel.SortDecktop(1-tp,1-tp,3)
 		end
