a8ebbc533cf5666c01ca3b415c2019769df5989d
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-01-16 13:41:18 +0100

    fixed Prime Tractiger

--- Rush/Transamu of Transcendence - 160016XXX/c160016005.lua
+++ Rush/Transamu of Transcendence - 160016XXX/c160016005.lua
@@ -1,12 +1,12 @@
 -- プライム・トラクタイガー
--- Praime Tiger
+-- Praime Tractiger
 local s,id=GetID()
 function s.initial_effect(c)
 	--summon/set with 1 tribute
 	local e0=aux.AddNormalSummonProcedure(c,true,true,1,1,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0),nil,s.otop)
 	-- Give Piercing damage to a  monster
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -34,24 +34,23 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
 end
 function s.filter(c)
-	return c:IsAttribute(ATTRIBUTE_WIND) and c:CanGetPiercingRush()
+	return c:IsFaceup() and c:CanGetPiercingRush()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	-- Requirement
+	--Requirement
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local tg=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil)
-	if Duel.SendtoGrave(tg,REASON_COST)==1 then
-		-- Effect
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
-		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
-		if #g>0 then
-			Duel.HintSelection(g,true)
-			-- Piercing
-			g:GetFirst():AddPiercing(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,c)
-		end
+	if Duel.SendtoGrave(tg,REASON_COST)<1 then return end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g,true)
+		--Piercing
+		g:GetFirst():AddPiercing(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,c)
 	end
 end
