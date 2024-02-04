1d3b1f07bf76117b22d699128859dc9c8cad100c
Naim <naimsantos45@gmail.com>
2024-02-04 08:09:59 -0300

    Add INFO-JP05 "Millennium Fiend Reflection"

---
+++ INFO-JP/c101205005.lua
@@ -0,0 +1,83 @@
+--ミレニアム・アブソリューター
+--Millennium Fiend Reflection
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Neither monster can be destroyed by battle
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(s.indestg)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Place this card in the Spell/Trap Zone as a Continuous Spell
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.sttg)
+	e2:SetOperation(s.stop)
+	c:RegisterEffect(e2)
+	--Special Summon itself if the opponent activates a card or effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_RECOVER)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_CHAIN_SOLVED)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+end
+function s.indestg(e,c)
+	local handler=e:GetHandler()
+	return c==handler or c==handler:GetBattleTarget()
+end
+function s.sttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.stop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
+		--Change Type to a Continuous Spell
+		local e1=Effect.CreateEffect(c)
+		e1:SetCode(EFFECT_CHANGE_TYPE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET)
+		e1:SetValue(TYPE_SPELL|TYPE_CONTINUOUS)
+		c:RegisterEffect(e1)
+	end
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and e:GetHandler();IsContinuousSpell()
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+	--local _,lp=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil):GetMaxGroup(Card.GetAttack)
+	--Duel.SetPossibleOperationInfo(0,CATEGORY_RECOVER,nil,1,tp,lp)
+end
+function s.atkfilter(c)
+	return c:IsFaceup() and c:GetAttack()>0
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
+			g=g:Select(tp,1,1,nil)
+			Duel.Recover(tp,g:GetFirst():GetAttack(),REASON_EFFECT)
+		end
+	end
+end
