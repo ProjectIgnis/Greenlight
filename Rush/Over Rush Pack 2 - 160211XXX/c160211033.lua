dd315762c7680c6ecbcf4cbaf05d425d7b23bb8a
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-02-22 11:40:49 +0100

    added Beta the Eternal Magnet Warrior

--- Rush/Over Rush Pack 2 - 160211XXX/c160211033.lua
+++ Rush/Over Rush Pack 2 - 160211XXX/c160211033.lua
@@ -1,9 +1,9 @@
---永久磁石の戦士α
---Alpha the Eternal Magnet Warrior
+--永久磁石の戦士β
+--Beta the Eternal Magnet Warrior
 --Scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	--Change name to "Alpha the Magnet Warrior" and Fusion Summon
+	--Change name to "Beta the Magnet Warrior" and Fusion Summon
 	local params = {nil,s.matfilter}
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation(Fusion.SummonEffTG(table.unpack(params)),Fusion.SummonEffOP(table.unpack(params))))
 	c:RegisterEffect(e1)
 end
-s.listed_names={99785935}
+s.listed_names={39256679}
 function s.matfilter(c)
 	return c:IsLocation(LOCATION_HAND|LOCATION_MZONE) and c:IsRace(RACE_ROCK) and c:IsAbleToGrave()
 end
@@ -24,7 +24,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return not c:IsCode(99785935) end
+	if chk==0 then return not c:IsCode(39256679) end
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_FUSION_SUMMON,nil,1,tp,0)
 end
@@ -38,7 +38,7 @@ function s.operation(fustg,fusop)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_CHANGE_CODE)
-		e1:SetValue(99785935)
+		e1:SetValue(39256679)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 		c:RegisterEffectRush(e1)
 		if fustg(e,tp,eg,ep,ev,re,r,rp,0) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
