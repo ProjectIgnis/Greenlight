d63bc86b9d249828e9cca9ac42cb7ae3c75ab7a1
Naim <naimsantos45@gmail.com>
2024-03-11 11:09:25 -0300

    Fiendsmith Requiem

---
+++ INFO-JP/c101205046.lua
@@ -0,0 +1,94 @@
+--刻まれし魔の鎮魂棺
+--Fiendsmith Requiem
+--Scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Link Summon Procedure
+	Link.AddProcedure(c,s.matfilter,1,1)
+	--Can only be Special Summoned once per turn
+	c:SetSPSummonOnce(id)
+	--Special Summon 1 "Fiendsmith" monster from your hand or Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(function() return Duel.IsMainPhase() end)
+	e1:SetCost(aux.selfreleasecost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Equip itself from GY to 1 warrior monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_EQUIP)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.equiptg)
+	e2:SetOperation(s.equipop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_FIENDSMITH}
+function s.matfilter(c,lc,sumtype,tp)
+	return c:IsRace(RACE_FIEND,lc,sumtype,tp) and c:IsAttribute(ATTRIBUTE_LIGHT,lc,sumtype,tp)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_FABLED) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetMZoneCount(tp,e:GetHandler())>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)
+	end
+end
+function s.equipfilter(c)
+	return not c:IsLinkMonster() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_FIEND) --and c:IsFaceup()
+end
+function s.equiptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.equipfilter(chkc) and chkc~=c end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(s.equipfilter,tp,LOCATION_MZONE,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,s.equipfilter,tp,LOCATION_MZONE,0,1,1,c)
+	local c=e:GetHandler()
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,c,1,tp,0)
+	--[[
+	if c:IsLocation(LOCATION_GRAVE) then
+		Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,tp,0)
+	end
+	--]]
+end
+function s.equipop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if not (Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(tp)) then
+		Duel.SendtoGrave(c,REASON_RULE,nil,PLAYER_NONE)
+	elseif Duel.Equip(tp,c,tc) then
+		--Equip limit
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		e1:SetValue(function(e,c) return c==e:GetLabelObject() end)
+		e1:SetLabelObject(tc)
+		c:RegisterEffect(e1)
+		--Increase ATK
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_EQUIP)
+		e2:SetCode(EFFECT_UPDATE_ATTACK)
+		e2:SetValue(600)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+		c:RegisterEffect(e2)
+	end
+end
