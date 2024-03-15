9fb06e4e504e55ca13fc51189e9ae596fa36e5b8
Naim <naimsantos45@gmail.com>
2024-03-15 08:27:17 -0300

    Add "Trickstar Colchica"

---
+++ DP29/c100437053.lua
@@ -0,0 +1,54 @@
+--トリックスター・コルチカ
+--Trickstar Colchica
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Link Summon procedure
+	Link.AddProcedure(c,s.matfilter,1,1)
+	--Can only be Special Summoned once per turn
+	c:SetSPSummonOnce(id)
+	--Inflict damage equal to the ATK of a monster destroyed by battle
+	local e2=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET,EFFECT_FLAG2_CHECK_SIMULTANEOUS)
+	e2:SetCode(EVENT_BATTLE_DESTROYED)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.damcond)
+	e2:SetCost(aux.selfbanishcost)
+	e2:SetTarget(s.damtg)
+	e2:SetOperation(s.damop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_TRICKSTAR}
+function s.matfilter(c,lc,sumtype,tp)
+	return c:IsSetCard(SET_TRICKSTAR,lc,sumtype,tp) and not c:IsType(TYPE_LINK,lc,sumtype,tp)
+end
+function s.damcfilter(c,tp,ct)
+	local bc=c:GetBattleTarget()
+	return (c:IsPreviousControler(tp) and c:IsPreviousSetCard(SET_TRICKSTAR))
+		or (ct==1 and bc and bc:IsControler(tp) and bc:IsSetCard(SET_TRICKSTAR))
+end
+function s.damcond(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.damcfilter,1,nil,tp,#eg)
+end
+function s.damtgfilter(c,e)
+	return c:IsCanBeEffectTarget(e) and c:GetBaseAttack()>0
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return eg:IsContains(chkc) and s.damtgfilter(chkc,e) end
+	if chk==0 then return eg:IsExists(s.damtgfilter,1,nil,e) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local tg=eg:FilterSelect(tp,s.damtgfilter,1,1,nil,e)
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,tg:GetFirst():GetBaseAttack())
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:GetBaseAttack()>0 then
+		Duel.Damage(1-tp,tc:GetBaseAttack(),REASON_EFFECT)
+	end
+end
