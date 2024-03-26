8b1445da90e81ec3660eacbe55692b4c5639f5af
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-03-26 11:11:54 +0200

    Summon Sorceress errata updates/fixes

--- VJMP/c61665245.lua
+++ VJMP/c61665245.lua
@@ -1,17 +1,17 @@
 --サモン・ソーサレス
---Summon Sorceress (errata)
+--Summon Sorceress
 local s,id=GetID()
 function s.initial_effect(c)
-	Link.AddProcedure(c,aux.NOT(aux.FilterBoolFunctionEx(Card.IsType,TYPE_TOKEN)),2,99,s.lcheck)
-	--Link Summon Procedure
 	c:EnableReviveLimit()
-	--Special Summon monster(s) when Link Summoned
+	--Link Summon Procedure
+	Link.AddProcedure(c,aux.NOT(aux.FilterBoolFunctionEx(Card.IsType,TYPE_TOKEN)),2,3,s.lcheck)
+	--Special Summon 1 monster from your hand in Defense Position, to your opponent's zone this card points to
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) end)
 	e1:SetTarget(s.sptg)
@@ -21,70 +21,52 @@ end
 function s.lcheck(g,lc,sumtype,tp)
 	return g:CheckSameProperty(Card.GetRace,lc,sumtype,tp)
 end
-function s.spfilter1(c,e,tp,zone)
+function s.hspfilter(c,e,tp,zone)
 	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,1-tp,zone)
-end
-function s.spfilter2(c,e,tp,rac)
-	return c:IsRace(rac) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local zone=e:GetHandler():GetLinkedZone(1-tp)
 	if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)>0
-		and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_HAND,0,1,nil,e,tp,zone) end
+		and Duel.IsExistingMatchingCard(s.hspfilter,tp,LOCATION_HAND,0,1,nil,e,tp,zone) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
+function s.dspfilter(c,e,tp,rac)
+	return c:IsRace(rac) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
 	local zone=c:GetLinkedZone(1-tp)
 	if Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sc=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp,zone):GetFirst()
-	if Duel.SpecialSummonStep(sc,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE,zone) then
+	local hsc=Duel.SelectMatchingCard(tp,s.hspfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp,zone):GetFirst()
+	if hsc and Duel.SpecialSummonStep(hsc,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE,zone) then
 		--Negate its effects
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-		sc:RegisterEffect(e1,true)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
-		sc:RegisterEffect(e2,true)
+		hsc:NegateEffects(c)
 	end
-	Duel.SpecialSummonComplete()
-	local rac=sc:GetRace()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_DECK,0,1,nil,e,tp,rac)
-		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tc=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_DECK,0,1,1,nil,e,tp,rac):GetFirst()
-		if tc then
-			Duel.BreakEffect()
-			if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
-				--Negate its effects
-				local e3=Effect.CreateEffect(c)
-				e3:SetType(EFFECT_TYPE_SINGLE)
-				e3:SetCode(EFFECT_DISABLE)
-				e3:SetReset(RESET_EVENT|RESETS_STANDARD)
-				tc:RegisterEffect(e3,true)
-				local e4=Effect.CreateEffect(c)
-				e4:SetType(EFFECT_TYPE_SINGLE)
-				e4:SetCode(EFFECT_DISABLE_EFFECT)
-				e4:SetReset(RESET_EVENT|RESETS_STANDARD)
-				tc:RegisterEffect(e4,true)
-			end
-			Duel.SpecialSummonComplete()
-			--Cannot only Special Summon monster(s) with the same original type as the summoned monster
-			local e5=Effect.CreateEffect(c)
-			e5:SetDescription(aux.Stringid(id,3))
-			e5:SetType(EFFECT_TYPE_FIELD)
-			e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-			e5:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-			e5:SetTargetRange(1,0)
-			e5:SetTarget(function(e,cc) return not cc:IsRace(tc:GetOriginalRace()) end)
-			e5:SetReset(RESET_PHASE|PHASE_END)
-			Duel.RegisterEffect(e5,tp)
-		end
+	if Duel.SpecialSummonComplete()==0 then return end
+	local rac=hsc:GetRace()
+	if not (Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.dspfilter,tp,LOCATION_DECK,0,1,nil,e,tp,rac)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1))) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local dsc=Duel.SelectMatchingCard(tp,s.dspfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,rac):GetFirst()
+	if not dsc then return end
+	Duel.BreakEffect()
+	if Duel.SpecialSummonStep(dsc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+		--Negate its effects
+		dsc:NegateEffects(c)
 	end
+	if Duel.SpecialSummonComplete()==0 then return end
+	--Cannot Special Summon, except monsters with the same original Type as the monster Special Summoned from the Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(e,c) return not c:IsOriginalRace(dsc:GetOriginalRace()) end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 end
