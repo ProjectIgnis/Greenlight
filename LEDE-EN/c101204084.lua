e305755e051a181772cb444a92745b79a5a13c12
Hatter <that.hatter@proton.me>
2024-04-15 04:33:16 +0800

    add "Krishnerd Witch"

---
+++ LEDE-EN/c101204084.lua
@@ -0,0 +1,87 @@
+--
+--Krishnerd Witch
+--Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	--Cannot be destroyed by opponent's card effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.indcon)
+	e1:SetValue(aux.indoval)
+	c:RegisterEffect(e1)
+	--Special Summon this card
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(function(_,_,eg) return eg:IsExists(s.spconfilter,1,nil) end)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--Shuffle 1 card to the Deck
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.tdcon)
+	e3:SetTarget(s.tdtg)
+	e3:SetOperation(s.tdop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={101204086}
+function s.indcon(e)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,101204086),0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+end
+function s.spconfilter(c)
+	return c:IsPreviousLocation(LOCATION_FZONE) and c:IsReason(REASON_EFFECT)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:GetHandler():IsOnField() and re:IsActiveType(TYPE_FIELD) and not re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.spfilter(c,e,tp)
+	if not c:IsCanBeSpecialSummoned(e,0,tp,false,false) then return false end
+	for _,code in ipairs(c.listed_names) do
+		if TYPE_FIELD&Duel.GetCardTypeFromCode(code)==TYPE_FIELD then return true end
+	end
+	return false
+end
+function s.tdfilter(c,e,tp)
+	return c:IsFaceup() and c:IsMonster() and (c:IsAbleToDeck() or s.spfilter(c,e,tp))
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_REMOVED|LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED|LOCATION_GRAVE)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local tc=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+	if not tc then return end
+	if s.spfilter(tc,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	else
+		Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
