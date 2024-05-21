4783f00cde517946b84e051dbaeeb5d2d73aa28c
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-05-21 10:05:25 +0200

    fixed Elemental HERO Flame Wingman

--- Rush/Structure Deck Birth of Hero - 160320XXX/c160320001.lua
+++ Rush/Structure Deck Birth of Hero - 160320XXX/c160320001.lua
@@ -6,17 +6,25 @@ function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,21844576,58932615)
-	--damge
+	--spsummon condition
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_BATTLE_DESTROYED)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetOperation(s.damop)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.fuslimit)
 	c:RegisterEffect(e1)
+	--damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EVENT_BATTLE_DESTROYING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.damop)
+	c:RegisterEffect(e2)
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local bc=e:GetHandler():GetBattleTarget()
-	if bc then
+	if bc and bc:IsLocation(LOCATION_GRAVE) then
 		local atk=bc:GetTextAttack()
 		if bc:WasMaximumMode() then
 			atk=bc:GetMaximumAttack()
