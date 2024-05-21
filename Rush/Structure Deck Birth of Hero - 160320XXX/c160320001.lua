4d3e97f415ed6221f5b83ef4964db6a16872b9d1
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-05-21 10:22:31 +0200

    added Elemental HERO Burst Wingman

--- Rush/Structure Deck Birth of Hero - 160320XXX/c160320001.lua
+++ Rush/Structure Deck Birth of Hero - 160320XXX/c160320001.lua
@@ -4,15 +4,9 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
+	c:AddMustBeFusionSummoned()
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,21844576,58932615)
-	--spsummon condition
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(aux.fuslimit)
-	c:RegisterEffect(e1)
 	--damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
